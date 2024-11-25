3935694c6c263eeb9df537198a6c2a255d757920
taesu0608 <taesu0608@gmail.com>
2024-11-25 16:45:37 +0900

    2. 상속 구현 - instanceof 사용

--- inheritance/Lion.java
+++ inheritance/Lion.java
@@ -10,5 +10,13 @@ public class Lion extends Animal{
 
     public static void main(String[] args) {
         Lion lion = new Lion("Lion");
+        Animal animal = new Animal("Animal");
+        //instanceof 메서드 활용
+        boolean l1 = lion instanceof Lion;
+        boolean l2 = lion instanceof Animal;
+        boolean a1 = animal instanceof Lion;
+        boolean a2 = animal instanceof Animal;
+        System.out.println("Lion? Lion; Animal;" + l1 +" "+l2);
+        System.out.println("Animal? Lion; Animal;" + a1 +" "+a2);
     }
 }
