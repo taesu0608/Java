8479625075a18bc748990b654dafd0e2337eb6c6
taesu0608 <taesu0608@gmail.com>
2024-11-28 12:26:41 +0900

    2. 상속 구현 - instance of 예제 구현   1) 부모, 자식 클래스 간의 instance of 확인   2) 인터페이스의 instance of 확인   3) 조부모 클래스 간의 instance of 확인

--- inheritance/Lion.java
+++ inheritance/Lion.java
@@ -1,6 +1,7 @@
 package inheritance;
 
-public class Lion extends Animal{
+public class Lion extends Animal implements Being{
+
     Lion(String name) {
         super(name);
     }
@@ -11,12 +12,25 @@ public class Lion extends Animal{
     public static void main(String[] args) {
         Lion lion = new Lion("Lion");
         Animal animal = new Animal("Animal");
-        //instanceof 메서드 활용
+        // 부모클래스, 자식클래스 간의 instanceof 값 확인
         boolean l1 = lion instanceof Lion;
         boolean l2 = lion instanceof Animal;
+        // 부모클래스, 자식클래스 간의 instanceof 값 확인
         boolean a1 = animal instanceof Lion;
         boolean a2 = animal instanceof Animal;
-        System.out.println("Lion? Lion; Animal;" + l1 +" "+l2);
-        System.out.println("Animal? Lion; Animal;" + a1 +" "+a2);
+        // interface의 instanceof 값 확인
+        boolean i1 = lion instanceof Being;
+        boolean i2 = animal instanceof Being;
+        // 조부모 클래스의 instanceof 값 확인
+        boolean o1 = lion instanceof Object;
+        boolean o2 = animal instanceof Object;
+
+        System.out.println("lion? Lion; Animal; " + l1 +" "+l2);
+        System.out.println("animal? Lion; Animal; " + a1 +" "+a2);
+        System.out.println("lion? (Interface) Being; " + i1);
+        System.out.println("animal? (Interface) Being; " + i2);
+        System.out.println("lion? (Interface) Object; " + o1);
+        System.out.println("animal? (Interface) Object; " + o2);
+
     }
 }
