55cf039c5620bf69bed70658e07809fd9bac380f
taesu0608 <taesu0608@gmail.com>
2025-01-01 01:13:20 +0900

    2. 정렬 1) 버블정렬 주석 첨부

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -1,22 +1,30 @@
 package Sorting;
 
 public class BubbleSort {
-
+    //배열을 뒷부분부터 채우기
     void bubblesort(int[] arr) {
-        //배열을 뒷부분부터 채우기
+
+        // 입력된 배열의 크기 = n
+        // 배열의 마지막 index = n - 1;
+        // 아직 length와 size
         int n = arr.length;
         for (int i = 0; i < n - 1; i++) {
+            // 배열의 마지막 - 1 부터 i + 1 까지 step 1만큼 감소하며 반복
             for (int j = n - 1; j > i; j--) {
+                // j와 j - 1을 비교 및 교환
                 if(arr[j - 1] > arr[j])
                     swap(arr, j - 1, j);
             }
         }
     }
-
+    // 배열을 앞부분부터 채우기
     void bubblesort2(int[] arr) {
-        // 배열을 앞부분부터 채우기
+
+        // 입력된 배열의 크기 = n
+        // 0에서 부터 배열의 끝 - 1 까지
         int n = arr.length;
         for (int i = 0; i < n - 1; i++) {
+            // j와 j + 1을 비교 및 교환
             for (int j = 0; j <= i; j++) {
                 if(arr[j + 1] < arr[j])
                 {
