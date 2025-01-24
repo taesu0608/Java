ec0f66d353bc8a39fbc712ec8df6f2d6e78abab8
taesu0608 <taesu0608@gmail.com>
2025-01-24 17:30:23 +0900

    3. 예약어 1) final 대상에 따른 작동 차이

---
+++ Enumeration/Enumeration.java
@@ -0,0 +1,47 @@
+package Enumeration;
+/*상수 선언 방법
+    1. final static
+    2. interface
+    3. 자체 클래스
+    - 자체클래스 Primitive Type이 아닌 Reference Type으로 상수를 다룸
+*/
+
+// 2. interface
+// interface의 특징 매개변수는 final static 생략가능
+interface EnumInterface {
+    int MONDAY = 1;
+    int TUESDAY = 2;
+    int WEDNESDAT = 3;
+    int THURSDAY = 4;
+    int FRIDAY = 5;
+    int SATURDAY = 6;
+    int SUNDAY = 7;
+}
+
+// 3. 자체클래스
+//객체의 비교임으로 값을 통한 비교가 어려워짐
+class Day{
+    public final static Day MONDAY = new Day();
+    public final static Day TUESDAY = new Day();
+    public final static Day WEDNESDAY = new Day();
+}
+public class Enumeration {
+
+    private final static int MONDAY = 1;
+    private final static int TUESDAY = 2;
+    private final static int WEDNESDAT = 3;
+    private final static int THURSDAY = 4;
+    private final static int FRIDAY = 5;
+    private final static int SATURDAY = 6;
+    private final static int SUNDAY = 7;
+
+    public static void main(String[] args) {
+        // 정수값인 상수의 문제 발생 -> 이를 해결하기 위해 자체 클래스 상수
+        if(MONDAY == 1)
+        {
+            System.out.println("월요일 입니다.");
+        }
+        // switch문에서 클래스활용이 어려움
+        // TODO: 변수명을 가져오는 reflection을 활용해야함
+    }
+}
---
+++ reservedWord/finalRW/Final01.java
@@ -0,0 +1,93 @@
+package reservedWord.finalRW;
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
+
+public class Final01 {
+/*
+    1. 멤버 final
+        1 - 1. 포인터 방식의 직접 변환 UnsafeClass()
+        1 - 2. 필드 fianl 멤버와 로컬 final 멤버
+    2. 메서드 final
+        - 상속시 overide 방지
+        - 공통 기능, 메서트 템플릿등 변경 사유가 없는 경우 주로 사용
+            cf) 강제로 final 메서드 변경하기
+                a. 오버로딩으로 같은명의 메서드
+                b. 조합(Composition) 사용
+                c. 리플렉션(Reflection) 라이브러리
+                d. Proxy를 활용한 객체 탈취
+    3. 클래스 final
+        - 상속을 금지
+    */
+
+    // 필드 fianl
+    static final int a = 1;
+    public static void main(String[] args) throws Exception{
+
+        // 1 - 1. 포인터와 같이 직접 참조 변환
+        Final01 f = new Final01();
+        f.UnsafeClass();
+
+        // 1 - 2. 필드 final a, 로컬 final b 둘다 출력
+        final int b = 2;
+        System.out.println(a + "," + b);
+
+        // 2 - 1. 메서드 Final
+        Final02 f2 = new Final02();
+        f.finalMethod();
+        f2.finalMethod(a);
+        f2.compFinalMethod();
+
+        //
+
+    }
+    public void UnsafeClass() throws Exception{
+        /*
+        1 - 1. 주소값을 통한 변환
+        주소값을 가져와서 값을 변경해보자
+            - JNI(Java Native Interface)를 사용하여 C++로 포인터 사용
+            - Unsafe 클래스 사용
+        */
+        Field unsafeField = Unsafe.class.getDeclaredField("theUnsafe");
+        unsafeField.setAccessible(true);
+        Unsafe unsafe = (Unsafe) unsafeField.get(null);
+
+        // 메모리 할당
+        long memoryAddress = unsafe.allocateMemory(4L);
+
+        // 메모리에 값 쓰기
+        unsafe.putInt(memoryAddress, 42);
+
+        // 메모리에서 값 읽기
+        int value = unsafe.getInt(memoryAddress);
+        System.out.println("Value at memory address: " + value); // 출력: 42
+
+        // 메모리 해제
+        unsafe.freeMemory(memoryAddress);
+    }
+
+    final public void finalMethod(){
+        System.out.println("this is final method");
+    }
+}
+
+class Final02 extends Final01{
+    /*
+    cf) 강제로 final 메서드 변경하기
+    a. 오버로딩으로 같은명의 메서드
+    b. 조합(Composition) 사용
+    TODO: c. 리플렉션(Reflection) 라이브러리를 활용한 final 회피
+    TODO: d. Proxy를 활용한 객체 탈취
+    */
+
+    public void finalMethod(int a){
+        //a. 오버로딩
+        System.out.println("this is overloaded final method");
+    }
+
+    public void compFinalMethod(){
+        System.out.println("this is composited final method");
+        finalMethod();
+    }
+}
+
