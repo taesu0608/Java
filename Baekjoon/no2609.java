dec32a8c4696fe0a67149dc56eed58b98c1f20d9
taesu <taesu0608@gmail.com>
2025-06-24 19:28:13 +0900

    refactor baekjoon no2609

--- Baekjoon/no2609.java
+++ Baekjoon/no2609.java
@@ -55,4 +55,26 @@ public class no2609 {
 
         System.out.println(gcd + "\n"+ lcm_b);
     }
+
+    public static void another(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] arr = br.readLine().split(" ");
+        int a = Math.max(Integer.parseInt(arr[0]), Integer.parseInt(arr[1]));
+        int b = Math.min(Integer.parseInt(arr[0]), Integer.parseInt(arr[1]));
+
+        System.out.println(gcd(a,b)+"\n"+lcm(a,b));
+
+
+    }
+
+    public static int gcd(int a, int b) {
+          if (b==0) return a;
+        return gcd(b, a % b);
+    }
+
+    public static int lcm(int a, int b) {
+        if (b==0) return a;
+        return (a*b)/gcd(a , b);
+    }
 }
