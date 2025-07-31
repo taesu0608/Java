3ac21f10eadbdd872f3ab333c055a3135178cd83
taesu <taesu0608@gmail.com>
2025-07-31 17:34:56 +0900

    chore: legnth 주석 추가

--- Arrays/Array/Length.java
+++ Arrays/Array/Length.java
@@ -4,5 +4,7 @@ public class Length {
     public static void main(String[] args) {
         int[] i = {0,1,2,3,4};
         System.out.println("배열의 크기가 5인 배열의 length 값 %d".formatted(i.length,i));
+
+        //출력 결과: 5
     }
 }
